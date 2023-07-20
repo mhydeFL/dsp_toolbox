6cd5e35365309630e711f341196db31581de1339
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-07-20 18:57:29 -0400

    First Commit

---
+++ lib/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pid_optimization",
+    version="0.0.0",
+    description="Auto-Tuning PID Library",
+    author="Formlabs",
+    author_email="mikhail.hyde@formlabs.com",
+    url="https://github.com/Formlabs/mordor/tree/master/python/lib",
+    packages= [
+        "project_manager",
+        "optimizers"
+    ]
+)
